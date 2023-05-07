0c3f9a495305b6b78f7b50a8a95ef2af345f6a63
Szymon Ograbek <szymon.ograbek@monterail.com>
2023-05-07 12:28:02 +0200

    convert to edge function

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -10,6 +10,10 @@ import { HumanChatMessage, LLMResult } from "langchain/schema";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/next-auth";
 
+export const config = {
+  runtime: "edge",
+};
+
 export async function POST(request: Request) {
   const session = await getServerSession(authOptions);
 
